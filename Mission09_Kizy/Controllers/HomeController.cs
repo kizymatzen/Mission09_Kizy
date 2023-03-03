1a335cdd4bd7c04ee14a7dbb12f4b126ab496e02
Kizy Matzenbacher <kizymatzen@gmail.com>
2023-03-02 23:56:22 -0700

    Update

--- Mission09_Kizy/Controllers/HomeController.cs
+++ Mission09_Kizy/Controllers/HomeController.cs
@@ -3,14 +3,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Mission09_Kizy.Models;
+
 
 namespace Mission09_Kizy.Controllers
 {
     public class HomeController : Controller
     {
+        private IBookstoreRepository repo;
+
+        public HomeController (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
         public IActionResult Index()
         {
-            return View();
+            var book = repo.Projects.ToList();
+
+            return View(book);
         }
     }
 }
---
+++ Mission09_Kizy/Models/EFBookstoreRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_Kizy.Models
+{
+    public class EFBookstoreRepository : IBookstoreRepository
+    {
+        private BookstoreContext context { get; set; }
+
+        public EFBookstoreRepository (BookstoreContext temp)
+        {
+            context = temp;
+        }
+
+        public IQueryable<Project> Projects => (IQueryable<Project>)context.Projects;
+    }
+}
