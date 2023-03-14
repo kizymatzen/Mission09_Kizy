761ee47001bababaa1fc336c0007aff9132b7a49
Kizy Matzenbacher <kizymatzen@gmail.com>
2023-03-13 19:34:40 -0600

    Update

--- Mission09_Kizy/Pages/ShoppingCart.cshtml.cs
+++ Mission09_Kizy/Pages/ShoppingCart.cshtml.cs
@@ -4,18 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Mission09_Kizy.Models;
 
 namespace Mission09_Kizy.Pages
 {
     public class ShoppingCartModel : PageModel
     {
+        private IBookstoreRepository repo { get; set; }
+
+        public ShoppingCartModel (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public Basket basket { get; set; }
         public void OnGet()
         {
         }
 
-        public void OnPost()
+        public IActionResult OnPost(int bookId)
         {
+            Book b = repo.Books.FirstOrDefault(x => x.BookId == bookId);
 
+            basket = new Basket();
+            basket.AddItem(b, 1);
+
+            return RedirectToPage();
         }
     }
 }
